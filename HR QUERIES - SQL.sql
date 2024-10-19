20d6f20121b8da034ff7fd8bc759dcaf65b74e7b
Hegazy11 <67936709+Hegazy11@users.noreply.github.com>
2024-10-19 15:26:32 +0300

    Update HR QUERIES - SQL.sql

--- HR QUERIES - SQL.sql
+++ HR QUERIES - SQL.sql
@@ -1,4 +1,4 @@
-﻿
+
 SELECT 
     AVG(CAST(Age AS INT)) AS AvgAge, 
     MAX(CAST(Age AS INT)) AS HighestAge, 
@@ -57,15 +57,8 @@ SELECT
     AVG(CAST(Salary AS DECIMAL(10, 2))) AS AvgSalary
 FROM Employee
 GROUP BY MaritalStatus;
---7. Pie Chart of Marital Status + Correlation Between Status and Salaries
+--7. Correlation Between Status and Salaries
 
-
-SELECT 
-    MaritalStatus, 
-    AVG(CASE WHEN OverTime = 'Yes' THEN 1 ELSE 0 END) AS OvertimePercentage
-FROM Employee
-GROUP BY MaritalStatus;
---8. Correlation Between Marital Status and Overtime
 
 
 SELECT 
@@ -116,14 +109,9 @@ SELECT
 FROM Employee
 GROUP BY MaritalStatus;
 
---15. Correlation Between Years at the Company and Salaries
+--15. Correlation Between Years at the Company and martital status
 
-SELECT 
-    State, 
-    AVG(CAST(Age AS INT)) AS AvgAge
-FROM Employee
-GROUP BY State;
---16. State with Age
+
 
 SELECT 
     Department, 
@@ -157,18 +145,19 @@ GROUP BY E.MaritalStatus;
 
 
 SELECT 
-    PR.ManagerRating, 
-    AVG(CAST(E.YearsSinceLastPromotion AS INT)) AS AvgYearsSincePromotion
+    E.JobRole, PR.ManagerRating
+    ,AVG(CAST(E.YearsSinceLastPromotion AS INT)) AS AvgYearsSincePromotion
 FROM PerformanceRating PR
 JOIN Employee E ON PR.EmployeeID = E.EmployeeID
-GROUP BY PR.ManagerRating;
+GROUP BY  E.JobRole, PR.ManagerRating;
 --20. Correlation Between Manager Rating and Promotion
 
 
 SELECT 
     EmployeeID, 
-    (CAST(SelfRating AS INT) - CAST(ManagerRating AS INT)) AS RatingDifference
-FROM PerformanceRating;
+    AVG(SelfRating) SelfRating , AVG(ManagerRating) AS ManagerRating
+FROM PerformanceRating
+group by EmployeeID;
 --21. Difference Between Self-Rating and Manager Rating
 
 
@@ -183,10 +172,143 @@ GROUP BY PR.TrainingOpportunitiesTaken;
 
 
 
+/*Departments & Job_Role with Highest Salary*/
+select (Department) , 
+sum(salary) as Highest_salary
+from Employee 
+Group by Department 
+order by sum(salary) desc
+
+select (jobrole) ,
+AVG (salary) as Highest_salary
+from Employee 
+Group by jobrole  
+order by AVG (salary) desc
+
+
+/* Top 5 Employee Salaries */
+select top 5 FirstName , LastName,  department , Jobrole, salary
+from Employee	
+group by  FirstName , LastName,  department , Jobrole, salary
+order by Salary desc
+
+
+/*Ethnicity and Salary*/
+
+SELECT Ethnicity,EducationField,EducationLevel, 
+AVG(Salary) AS Highest_Salary
+FROM Employee
+JOIN EducationLevel ON Employee.Education = EducationLevel.EducationLevelID
+GROUP BY Ethnicity, EducationField, EducationLevel
+order by AVG(Salary) desc
 
 
 
+/*state by salary and count of empolyees*/
+
+SELECT State, 
+COUNT(EmployeeID) AS Employees, 
+SUM(CAST(Salary AS DECIMAL)) AS Total_Salary 
+FROM Employee
+GROUP BY State
+order by SUM(CAST(Salary AS DECIMAL)) desc
+
+
+WITH TotalEmployees AS (SELECT State, 
+COUNT(EmployeeID) AS TotalEmployees
+FROM Employee
+GROUP BY State)
+SELECT E.State, 
+COUNT(E.EmployeeID) AS Employees, 
+TotalEmployees,
+CAST(ROUND((COUNT(E.EmployeeID) * 100.0 / TotalEmployees), 3) AS DECIMAL(5,3)) AS Percentage,
+E.Ethnicity,
+SUM(CAST(E.Salary AS DECIMAL)) AS Total_Salary
+FROM Employee E
+JOIN TotalEmployees T ON E.State = T.State
+GROUP BY E.State, E.Ethnicity, TotalEmployees
+ORDER BY COUNT(E.EmployeeID)
+
+
+/*Correlation Between Education Levels and Salary*/
+SELECT 
+FirstName, 
+LastName, 
+EducationField, 
+JobRole,
+EducationLevel, 
+SUM(CAST(E.Salary AS DECIMAL)) AS Salary
+FROM 
+Employee E
+JOIN 
+EducationLevel ED ON E.Education = ED.EducationLevelID
+GROUP BY 
+FirstName, 
+LastName, 
+EducationField,
+JobRole,
+EducationLevel
+ORDER BY Salary
+
+/*Most Paid Role by Department*/
+select*
+from employee
+
+SELECT Department, JobRole, MAX(Salary) AS Highest_Salary
+FROM Employee 
+GROUP BY Department, JobRole
+ORDER BY MAX(Salary) DESC
+
+/*Pie Chart of Marital Status*/
+SELECT MaritalStatus, COUNT(EmployeeID) AS Employees
+FROM Employee
+GROUP BY MaritalStatus
+
+/*Correlation Between Marital Status and Salaries*/
+SELECT MaritalStatus,AVG(CAST(Salary AS DECIMAL(10, 2))) AS AverageSalary
+FROM Employee
+GROUP BY MaritalStatus
+
+/*Correlation Between MaritalStatus, Over Time and Distance from Home*/
+SELECT [DistanceFromHome (KM)], Overtime 
+FROM Employee
+GROUP BY [DistanceFromHome (KM)], Overtime
 
 
 
+/*Hire Date by Count of Employees*/
 
+SELECT 
+YEAR(HireDate) AS HireYear,COUNT(EmployeeID) AS EmployeeCount
+FROM Employee
+GROUP BY YEAR(HireDate)
+ORDER BY HireYear
+
+
+/* Count of Employees and Terminated Employees (Gender)*/
+SELECT 
+SUM(CASE WHEN Attrition = 'No' THEN 1 ELSE 0 END) AS ActiveEmployees,
+SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS TerminatedEmployees,
+COUNT(EmployeeID) AS TotalEmployees
+FROM 
+Employee 
+
+SELECT Gender,
+SUM(CASE WHEN Attrition = 'No' THEN 1 ELSE 0 END) AS ActiveEmployees,
+SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS TerminatedEmployees,
+COUNT(EmployeeID) AS TotalEmployees
+FROM Employee 
+GROUP BY Gender
+
+
+SELECT 
+    e.Gender,
+    COUNT(e.EmployeeID) AS TotalEmployees,
+    SUM(CASE WHEN e.Attrition = 'Yes' THEN 1 ELSE 0 END) AS TerminatedEmployees,
+    SUM(CASE WHEN e.Attrition = 'No' THEN 1 ELSE 0 END) AS ActiveEmployees,
+    CAST(SUM(CASE WHEN e.Attrition = 'Yes' THEN 1 ELSE 0 END) AS DECIMAL(10,2)) * 100.0 / COUNT(e.EmployeeID) AS TerminatedPercentage,
+    CAST(SUM(CASE WHEN e.Attrition = 'No' THEN 1 ELSE 0 END) AS DECIMAL(10,2)) * 100.0 / COUNT(e.EmployeeID) AS ActivePercentage
+FROM 
+    Employee e
+GROUP BY 
+    e.Gender;
